7f2eaba2cd65b816beed1230e8facfbc25cb49ba
cyberpapi <robdezendorf@gmail.com>
2026-01-18 11:47:13 -0500

    feat(enrichment): Export all processors from package

--- src/imessage_max/enrichment/__init__.py
+++ src/imessage_max/enrichment/__init__.py
@@ -3,6 +3,12 @@
 from .images import process_image
 from .videos import process_video
 from .audio import process_audio
-from .links import enrich_links
+from .links import enrich_link, enrich_links
 
-__all__ = ["process_image", "process_video", "process_audio", "enrich_links"]
+__all__ = [
+    "process_image",
+    "process_video",
+    "process_audio",
+    "enrich_link",
+    "enrich_links",
+]
